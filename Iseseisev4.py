3fe64487719b6bd4f8ab5ba4de6a4df3b13d2891
tammikott <144793453+tammikott@users.noreply.github.com>
2024-01-10 11:32:50 +0200

    Update Iseseisev4.py

--- Iseseisev4.py
+++ Iseseisev4.py
@@ -1,4 +1,7 @@
-#banner
+
+'''
+banner
+'''
 
 
 #def banner(l):
@@ -13,3 +16,72 @@
 #for i in range(kuvamine):
 #    banner(tekst)
 
+'''
+Õunamahl
+'''
+#def mahlapakkide_arv(kg):
+#    mahlapakkide_arv = kg * 0,4 / 3
+#    return mahlapakkide_arv
+
+
+#ountekogus = float(input("Sisesta õunte kogus: "))
+#print(mahlapakkide_arv(ountekogus))
+
+
+
+
+#mahlapakkide_arv()
+
+'''
+Pidu
+'''
+#def eelarve(arv):
+  #  kogusumma = arv * 10 + 55
+ #   return kogusumma
+
+
+
+
+
+#kylaliste_arv = int(input("Mitu inimest on kutsutud?: "))
+#kylaliste_arv_tegelik = int(input("Mitu inimest on tulid?: "))
+#print(f"MAX eelarve: {eelarve(kylaliste_arv)}")
+#print(f"MIN eelarve: {eelarve(kylaliste_arv_tegelik)}")
+
+
+
+#eelarve(arv)
+'''
+tervitused
+'''
+#def tervitus():
+#    print("võõrustaja: "Tere!"")
+#   print(f"Täna {jrk}, kord tervitada, mõtliseb võõrustaja")
+#    print("Külaline: Tere, suur tänu kutse eest!"")
+          
+
+#kylaliste_arv = int(input("Mitu inimest on kutsutud?: "))
+#for in in range(kylaliste_arv):
+
+
+#tervitus(i+1)
+
+'''
+mündid
+'''
+def sendid():
+
+failinimie = int(input("Mis faili nimi?: "))
+fail = open(failinimi, encoding="utf-8")
+
+
+
+
+
+
+
+
+sendid()
+'''
+kuupäev
+'''
