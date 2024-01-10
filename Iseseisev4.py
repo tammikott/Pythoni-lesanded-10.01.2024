a3c024cfe626bf29b7adb4f26352f5355123b6aa
tammikott <144793453+tammikott@users.noreply.github.com>
2024-01-10 11:16:53 +0200

    Update Iseseisev4.py

--- Iseseisev4.py
+++ Iseseisev4.py
@@ -1,14 +1,15 @@
 #banner
 
 
-def banner(l):
-    print(l.upper)
-kuvamine = int(input("Mitu korda sa lauset soovid kuvada?: "))
-tekst = input("Mis teksti tahad")
+#def banner(l):
+#    print(l.upper())
+#kuvamine = int(input("Mitu korda sa lauset soovid kuvada?: "))
+#tekst = input("Mis teksti tahad: ")
     
 
    
 
 
-for i in range(kuvamine):
-    banner(tekst)
+#for i in range(kuvamine):
+#    banner(tekst)
+
