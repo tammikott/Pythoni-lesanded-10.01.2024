7d780ab730693425a89d2fd963b6d3f48bcce29d
tammikott <144793453+tammikott@users.noreply.github.com>
2024-01-16 10:46:04 +0200

    kordamine

---
+++ kordamine.py
@@ -0,0 +1,20 @@
+#Kordamine
+#Ott Tammik 16.01.2024
+
+
+vanused = [12, 2, 1, 29, 34, 20, 18, 46, 13, 8, 24, 40, 5, 12, 11, 8,]
+
+def vanus(v):
+    
+    print("suurim vanus on" max(vanused))
+    print("väikseim vanus on" min(vanused))
+    kogusumma = sum(vanused)
+    print("kogusumma on" (kogusumma))
+    print("kesmine on" kogusumma/len(vanused))
+
+
+
+
+
+
+vanus(v)
