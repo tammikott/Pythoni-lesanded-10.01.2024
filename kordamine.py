3172764667c1c358564d2c58798a003b8af46095
tammikott <144793453+tammikott@users.noreply.github.com>
2024-01-30 09:20:45 +0200

    Update kordamine.py

--- kordamine.py
+++ kordamine.py
@@ -1,30 +1,64 @@
 #Kordamine
 #Ott Tammik 16.01.2024
 
+#  List Less Than Ten
+# 	Take a list and write a program that prints out all the elements of the list that are less than 5. 1p
+# 		a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+# 	Instead of printing the elements one by one, make a new list that has all the elements less than 5 from this list in it and print out this new list. 1p
+# 	Write this in one line of Python. 1p
+# 	Ask the user for a number and return a list that contains only elements from the original list a that are smaller than that number given by the user
 
-vanused = [12, 2, 1, 29, 34, 20, 18, 46, 13, 8, 24, 40, 5, 12, 11, 8,]
 
-def vanus():
+
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+def list(a)
+    
+
+
+
+
+
+
+
+
+list(a)
+
+
+
+
+
+
+
+#  Vanused
+# 	loo vanuste loend 1p
+# 	leia numbrite suurim ja vĆ¤ikseim arv  1p
+# 	kogusumma  1p
+# 	keskmine 1p
+
+
+
+vanused = [12, 2, 1, 29, 34, 20, 18, 46, 13, 8, 24, 40, 5, 12, 11, 8]
+
+def vanus(vanused):
     
     print("suurim vanus on", max(vanused))
     print("väikseim vanus on", min(vanused))
     kogusumma = sum(vanused)
     print("kogusumma on", (kogusumma))
-    print("kesmine on", kogusumma/len(vanused))
+    print("keskmine on", kogusumma/len(vanused))
+
+vanus(vanused)
 
 
 
-vanus()
-
-
-positiivne = [12, 5, 17, 6, 10, 14, 9, 7, 15, 23, 29, 11, 16, 18, 9, 25, 14, 8, ]
-negatiivne = [-1, -7, -11, -9, -14, -21, -31, -3, -18, -21, -24,]
-
-def loendid():
+# positiivne = [12, 5, 17, 6, 10, 14, 9, 7, 15, 23, 29, 11, 16, 18, 9, 25, 14, 8, ]
+# negatiivne = [-1, -7, -11, -9, -14, -21, -31, -3, -18, -21, -24,]
+# def loendid():
     
-    arvud = int(input("Kirjuta 5 positiivset või negatiivset arvu: "))
-    if arvud == "-":
-        print(negatiivne)
-    elif arvud == "0"
-    else:
-        print(positiivne)
+#     for i in range (5):
+#         arvud = int(input("Kirjuta 5 positiivset või negatiivset arvu: "))
+#     if 
+
+# loendid()
+
