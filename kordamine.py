b352813eda7870992be5717014c64756148557a0
tammikott <144793453+tammikott@users.noreply.github.com>
2024-02-13 11:31:37 +0200

    kordamine

--- kordamine.py
+++ kordamine.py
@@ -3,6 +3,94 @@
 
 import random
 
+#14. Palkade vordlus - Loo palk.txt fail tootajate nime, soo ja palganumbriga (10 tootajat).
+#Koosta programm, mis analuusib kas firmas toimub diskrimineerimist soo jargi.
+#Selleks vordle omavahel meeste ja naiste palkade keskmiseid,
+#samuti meeste ja naiste kĆµige kĆµrgemat palka. Programm peab tegema otsuse.
+#
+#	Hubert Hunt m 2340
+#	Siim Siil m 2570
+#	MĆ¤rt MĆ¤ger m 1960
+#	Vilma Vihmauss n 2060
+#	Merike Metskits n 2250
+#	Kati Karu n 2370
+#	Elmar Elevant m 2900
+#	Timoteus Tigu m 2850
+#	Reet Rebane n 2340
+#	Kalev Kaamel m 2570
+#	Karmen Kass n 2120
+#	Kornelius Koer m 2250
+
+def palgavordlus():
+    
+    with open("palgad.txt", "r", encoding="utf-8") as f:
+        loe = f.readlines()
+
+    meeste_palk = []
+    naiste_palk = []
+
+
+    for line in loe:
+        n, s, p = line.split(" ")
+
+        if s == "n":
+            naiste_palk.append(int(p))
+        else:
+            meeste_palk.append(int(p))            
+
+    keskmine_mehed = sum(meeste_palk)/len(meeste_palk)
+    keskmine_naised = sum(naiste_palk)(naiste_palk)
+    suurim_mehed = max(meeste_palk)
+    suurim_naised = max(naiste_palk)
+
+    if keskmine_mehed > keskmine_naised:
+        print("Mehed saavad rohkem palka")
+    elif keskmine_naised > keskmine_mehed:
+        print("Naised saavad rohkem palka")
+    else:
+        print("Palgad ei ole ebaõiglased")
+
+
+palgavordlus()
+
+
+
+#Eurokalkulaator
+#	Koosta programm, mis kalkuleerib valuuta vastavalt kasutaja soovile EUR->EEK vĆµi EEK->EUR.
+#	Oluline on kasutada kahte funktsiooni!!
+
+
+
+def eur_eek(summa):
+    return summa * 15.6466
+
+def eek_eur(summa):
+    return summa * 0.0639
+
+def pohifunktsioon():
+    print("Tahad teisendada EUR -> EEK või EEK -> EUR?")
+    kasutaja_valik = input("Sisesta 'eur2eek' või 'eek2eur': ")
+
+    if kasutaja_valik.lower() == "eur2eek":
+        summa = float(input("Sisesta summa eurodes: "))
+        teisendatud_summa = eur_eek(summa)
+        print(f"{summa} EUR on {teisendatud_summa} EEK.")
+
+
+
+    elif kasutaja_valik.lower() == "eek2eur":
+        summa = float(input("Sisesta summa Eesti kroonides: "))
+        teisendatud_summa = eek_eur(summa)
+        print(f"{summa} EEK on {teisendatud_summa} EUR.")
+
+
+
+
+    else:
+        print("Aru ei saanud või?")
+
+pohifunktsioon()
+
 
 
 
@@ -26,50 +114,7 @@ list()
 
 
 
-
-#14. Palkade vĆµrdlus - Loo palk.txt fail tĆ¶Ć¶tajate nime, soo ja palganumbriga (10 tĆ¶Ć¶tajat).
-#	Koosta programm, mis analĆ¼Ć¼sib kas firmas toimub diskrimineerimist soo jĆ¤rgi. Selleks vĆµrdle omavahel meeste ja naiste palkade keskmiseid, samuti meeste ja naiste kĆµige kĆµrgemat palka. Programm peab tegema otsuse.
-#
-#	Hubert Hunt m 2340
-#	Siim Siil m 2570
-#	MĆ¤rt MĆ¤ger m 1960
-#	Vilma Vihmauss n 2060
-#	Merike Metskits n 2250
-#	Kati Karu n 2370
-#	Elmar Elevant m 2900
-#	Timoteus Tigu m 2850
-#	Reet Rebane n 2340
-#	Kalev Kaamel m 2570
-#	Karmen Kass n 2120
-#	Kornelius Koer m 2250
-
-
-def palgavordlus():
-
-    fail = open("palgad.txt", encoding="UTF-8")
-    for rida in fail:
-        print("Need on tootajate palgad: " + rida)
-        
-
-
-
-palgavordlus()
-
-
-#Eurokalkulaator
-#	Koosta programm, mis kalkuleerib valuuta vastavalt kasutaja soovile EUR->EEK vĆµi EEK->EUR.
-#	Oluline on kasutada kahte funktsiooni!!
-
-
-
-
-
-
-
-
-
-
-#KaugushĆ¼pe
+#Kaugushupe
 #	kasutaja sisestab 3 kaugushĆ¼ppe tulemust - 1p
 #	sinu programm leiab ning vĆ¤ljastab parima ja keskmise tulemuse - 2p
 #	programmi dialoog kasutajaga on arusaadav ja Ć¼heselt mĆµistetav - 1p
@@ -90,7 +135,7 @@ def kaugushupe():
 kaugushupe()
 
 
-#TĆ¤ringud
+#Taringud
 #	kuvatakse korrektne arusaadav kĆ¼simus ja hiljem vastus - 1p
 #	kasutaja vĆµistleb kahe tĆ¤ringuga arvuti vastu - 1p
 #	kasutaja teeb pakkumise ning suurima tĆ¤ringupunktisumma vĆµitja saab laual oleva raha endale - 2p
@@ -181,16 +226,3 @@ def vanus(vanused):
     print("keskmine on", summa/len(vanused))
 
 vanus(vanused)
-
-
-
-# positiivne = [12, 5, 17, 6, 10, 14, 9, 7, 15, 23, 29, 11, 16, 18, 9, 25, 14, 8, ]
-# negatiivne = [-1, -7, -11, -9, -14, -21, -31, -3, -18, -21, -24,]
-# def loendid():
-    
-#for i in range (5):
-#    arvud = int(input("Kirjuta 5 positiivset või negatiivset arvu: "))
-#if 
-
-# loendid()
-
